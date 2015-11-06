495b94c0b5f1328c0ba540aac635daa83c14249f
Cwalinski, Matthew J <mcwalinski@mac.com>
2015-11-06 10:09:25 -0500

    Add required field for explanation

--- app/models/notes.js
+++ app/models/notes.js
@@ -5,6 +5,6 @@ var mongoose = require('mongoose');
 // define our model
 // module.exports allows us to pass this to other files when it is called.  This happens in routes.js
 module.exports = mongoose.model('notes', new mongoose.Schema({
-    title : {type : String, default: ''},
-    body  : {type : String, default: ''}
+    title : {type : String, default: '', required : false },
+    body  : {type : String, default: '', required : false }
 }));
